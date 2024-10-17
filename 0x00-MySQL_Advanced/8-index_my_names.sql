8b6c4aed652466b5b2a1bb44c7190fe0402bcd8a
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-10-17 13:58:34 +0300

    Update 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Add a generated column for the first letter of the name
 
 -- Create the index on the generated column
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
