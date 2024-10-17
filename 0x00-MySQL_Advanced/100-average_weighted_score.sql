5fdcbbf4381ae9cf412263dd2355a39b8ac5ddb6
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-10-17 08:33:09 +0300

    Create stored procedure to compute average weighted score for a user

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,15 @@
 -- Create stored procedure to compute average weighted score for a user
-DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_weighted_score FLOAT DEFAULT 0;
-    DECLARE total_weight INT DEFAULT 0;
-    
-    -- Calculate the total weighted score and total weight
-    SELECT SUM(c.score * p.weight) INTO total_weighted_score,
-           SUM(p.weight) INTO total_weight
-    FROM corrections c
-    JOIN projects p ON c.project_id = p.id
-    WHERE c.user_id = user_id;
-    
-    -- Calculate and update the average score
-    IF total_weight > 0 THEN
-        UPDATE users
-        SET average_score = total_weighted_score / total_weight
-        WHERE id = user_id;
-    ELSE
-        UPDATE users
-        SET average_score = 0
-        WHERE id = user_id;
-    END IF;
-END //
-
+    UPDATE users set average_score = (SELECT
+    SUM(corrections.score * projects.weight) / SUM(projects.weight)
+    FROM corrections
+    INNER JOIN projects
+    ON projects.id = corrections.project_id
+    where corrections.user_id = user_id)
+    where users.id = user_id;
+END $$
 DELIMITER ;
